25eb4046c11cad3fde30735cc2165502322e6317
SwiftyCody <soldolly@gmail.com>
2023-01-14 07:28:46 +0900

    First Commit

---
+++ FindCoronaCenter/FindCoronaCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCoronaCenterApp.swift
+//  FindCoronaCenter
+//
+//  Created by solgoon on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct FindCoronaCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
